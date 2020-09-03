f19a90d98d896c7584d9499a090764050f833703
Aryan-sys <ghanshyam.parsram@gmail.com>
2020-09-03 13:42:07 +0530

    food doesnt deduct for some reason

--- sketch.js
+++ sketch.js
@@ -1,13 +1,28 @@
+
 //Create variables here
 var database;
 var dog, dogNormal;
 var happyDog, foodS, foodStock;
+var feed, addFood;
+var fedTime, lastFed;
+var foodObj;
+
+
 
 function preload()
 {
   //load images here\
   dogNormal = loadImage("images/dogImg.png")
   dogHappy = loadImage("images/dogImg1.png");
+
+
+  feed = createButton("feed the dog");
+  feed.position(600, 700);
+  feed.mousePressed(dogFeed);
+
+  addFood = createButton("Add food");
+  addFood.position(800, 700);
+  addFood.mousePressed(Foodadd)
 }
 
 function setup() {
@@ -18,38 +33,66 @@ function setup() {
   dog.addImage(dogNormal);
   
 
- foodStock = database.ref('food');
- foodStock.on("value", readStock);
+ 
+
+foodObj = new Food();
+
+
 }
 
 
 function draw() {  
+  console.log(foodObj.foodStock)
 background(46,139,87);
-if(keyWentDown(UP_ARROW)){
-  writeStock(foodS);
-  dog.addImage(dogHappy);
-}
+
+foodObj.display();
+
+fedTime = database.ref('FeedTime');
+fedTime.on("value", function(data){
+  lastFed = data.val();
+ 
+})
+
+
   drawSprites();
   //add styles here
   textSize(20);
   fill("black");
   stroke(20);
-text("Food: " + foodS,500,50);
+  if(lastFed >= 12){
+    text("Last Fed :" + lastFed%12 + "PM", 600,50);
+  }else if(lastFed === 0){
+    text("Last Fed : 12 AM", 600,50);
+  }else{
+    text("Last Fed :" + lastFed%12 + "AM", 600,50);
+  }
+  
+text("Food: " +  foodObj.foodStock,500,50);
 textSize(15)
-text("Press the up arrow to feed the dog! Wait for the dog to tell you the food stock before feeding!",10,20);
+
 }
 
-function readStock(data){
+/*function readStock(data){
  foodS = data.val();
-}
-function writeStock(x){
-if(x<=0){
-  x = 0;
-}else{
-  x=x-1;
-}
+ console.log(fedTime)
+}*/
 
+function Foodadd(){
+  foodObj.foodStock++;
   database.ref('/').update({
-food:x
+    food: foodObj.foodStock
   })
+  
+}
+function dogFeed(){
+  hour();
+
+  dog.addImage(dogHappy)
+  foodObj.updateFoodStock(foodObj.getFoodStock());
+  database.ref('/').update({
+    food: foodObj.getFoodStock(),
+    FeedTime : hour()
+  })
+  
+  
 }
